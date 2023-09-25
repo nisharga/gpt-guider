5b4dae33f88a133bb2d6c25d3a59bc508d234908
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-09-25 17:43:36 +0600

    footer added

--- src/Pages/HomePage/Hero.jsx
+++ src/Pages/HomePage/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
                 <div className="text-white">
                     <h1 className="text-2xl md:text-4xl font-bold">Save Your Time <span className="text-[#35DC9D]">&</span> Study Faster</h1>
                     <div>
-                        <p className="text-sm md:text-[22px] md:leading-8 mt-3 md:mt-5">Increase your productivity, Elevate your learning experience with the most comprehensive and powerful AI tools.</p>
+                        <p className="text-base md:text-[22px] md:leading-8 mt-3 md:mt-5">Increase your productivity, Elevate your learning experience with the most comprehensive and powerful AI tools.</p>
                     </div>
                     <div className="mt-5 md:mt-8">
                         <button className="font-semibold bg-gradient-to-r from-[#00B6BD] to-[#ACFFAD] px-8 py-2 rounded-full">Get Started</button>
--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,8 +1,43 @@
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className="container mx-auto mt-16">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8 text-white mx-4 md:mx-0">
+        <div>
+          <img className="w-[128px] h-[32px]" src="https://i.ibb.co/j4DCXgn/Frame.png" alt="" />
+        </div>
+        <div>
+          <p className="text-base md:text-lg font-bold">Home</p>
+          <div className="text-sm md:text-base mt-5">
+            <p className="mb-4">Features</p>
+            <p className="mb-4">Pricing</p>
+            <p className="mb-4">About us</p>
+          </div>
+        </div>
+        <div>
+          <p className="text-base md:text-lg font-bold">Terms</p>
+          <div className="text-sm md:text-base mt-5">
+            <p className="mb-4">Privacy Policy</p>
+            <p className="mb-4">Terms and Conditions</p>
+            <p className="mb-4">Copyright Policy</p>
+            <p className="mb-4">Security</p>
+          </div>
+        </div>
+        <div>
+          <p className="text-base md:text-lg font-bold">Support</p>
+          <div className="text-sm md:text-base mt-5">
+            <p className="mb-4">FAQs</p>
+            <p className="mb-4">Contact us</p>
+            <p className="mb-4">Help</p>
+          </div>
+        </div>
+        <div>
+          <p className="text-base md:text-lg font-bold">Follow us </p>
+          <div className="mt-5">
+            
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
