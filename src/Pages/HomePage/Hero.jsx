b98e731a96c9c170b4dfa9e002c0bfdd42dc0571
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-09-25 19:58:19 +0600

    all users

--- src/Pages/HomePage/Hero.jsx
+++ src/Pages/HomePage/Hero.jsx
@@ -13,6 +13,20 @@ const Hero = () => {
                     </div>
                 </div>
             </div>
+            <div>
+                <div className="mt-24 flex items-center">
+                    <div className="flex">
+                        <img className="w-[36px] h-[36px] rounded-full relative z-10" src="https://i.ibb.co/ssKPxsF/Avatar-Image.png" alt="" />
+                        <img className="w-[36px] h-[36px] rounded-full relative z-20 -left-4" src="https://i.ibb.co/YdjBJzL/Avatar-Image-1.png" alt="" />
+                        <img className="w-[36px] h-[36px] rounded-full relative z-30 -left-8" src="https://i.ibb.co/TcZ8Jx6/Avatar-Image-2.png" alt="" />
+                        <img className="w-[36px] h-[36px] rounded-full relative z-40 -left-12" src="https://i.ibb.co/C0p5jQq/Avatar-Image-3.png" alt="" />
+                        <img className="w-[36px] h-[36px] rounded-full relative z-50 -left-16" src="https://i.ibb.co/JrPbc7C/Avatar-Image-4.png" alt="" />
+                    </div>
+                    <div>
+                        <p className="text-white text-lg">Trusted by more than 100K <span className="bg-gradient-to-t from-[#8DF1AF] to-[#25C6BA] bg-clip-text text-transparent">users</span></p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
