494c5776460ebac391b235077dfcf7aeb703f616
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-09-26 15:57:53 +0600

    generator added

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -2,9 +2,11 @@ import AudioVideo from "./AudioVideo"
 import ChatWithPdf from "./ChatWithPdf"
 import DiagramPlugin from "./DiagramPlugin"
 import DocumentAi from "./DocumentAi"
+import Generator from "./Generator"
 import Hero from "./Hero"
 import PlugIn from "./PlugIn"
 import Prof from "./Prof"
+import Transcription from "./Transcription"
 
 const HomePage = () => {
   return (
@@ -16,6 +18,8 @@ const HomePage = () => {
       <ChatWithPdf />
       <AudioVideo />
       <DiagramPlugin />
+      <Transcription />
+      <Generator />
     </div>
   )
 }
